0f4d791d1b7b77424c17df3c6c600df04f2394fa
AMbonani <refilwe.given@gmail.com>
2023-10-27 00:40:54 +0200

    w04

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,4 +1,3 @@
-
 const myProfile = {};
 
 myProfile.name = "Abigail Maimela";
@@ -7,34 +6,36 @@ myProfile.favoriteFoods = ["chicken", "beef", "cake"];
 myProfile.hobbies = ["Reading", "outdoor activities", "watching movies"]; 
 myProfile.placesLived = []; 
 
-
 myProfile.placesLived.push({ place: "Erasmuskloof 1 year", length: "Elarduspark 3 yrs" });
 myProfile.placesLived.push({ place: "Atteridgeville 25 yrs", length: "pretoria north 5 yrs" });
 
-
 document.getElementById("name").textContent = myProfile.name;
+
 const photoImage = document.getElementById("photo");
 photoImage.setAttribute("src", myProfile.photo);
 photoImage.setAttribute("alt", myProfile.name);
 
-
 const favoriteFoodsList = document.getElementById("favorite-foods");
+favoriteFoodsList.innerHTML = ""; 
+
 myProfile.favoriteFoods.forEach((food) => {
   const listItem = document.createElement("li");
   listItem.textContent = food;
   favoriteFoodsList.appendChild(listItem);
 });
 
-
 const hobbiesList = document.getElementById("hobbies");
+hobbiesList.innerHTML = ""; 
+
 myProfile.hobbies.forEach((hobby) => {
   const listItem = document.createElement("li");
   listItem.textContent = hobby;
   hobbiesList.appendChild(listItem);
 });
 
-
 const placesLivedDL = document.getElementById("places-lived");
+placesLivedDL.innerHTML = ""; 
+
 myProfile.placesLived.forEach((place) => {
   const dt = document.createElement("dt");
   const dd = document.createElement("dd");
