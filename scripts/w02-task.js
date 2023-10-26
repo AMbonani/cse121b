934e09acd43064e187c2f91bd830b082a48a9780
AMbonani <refilwe.given@gmail.com>
2023-10-27 00:20:32 +0200

    modified w02

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,12 +1,23 @@
 
-const fullName = "Abigail Maimela";
+const fullName = "Abigail Maimela"; 
+
+
 const currentYear = new Date().getFullYear();
-const profilePicture = "images/myphoto.jpg";
 
 
+const profilePicture = "images/myphoto.jpg"; 
+
+.
 const nameElement = document.getElementById("name");
+
+
 const foodElement = document.getElementById("food");
+
+
 const yearElement = document.querySelector("#year");
+
+
+const imageElement = document.getElementById("profileImage");
 
 
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
@@ -15,24 +26,24 @@ nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 
 
-const imageElement = document.getElementById("profileImage");
 imageElement.setAttribute("src", profilePicture);
+
+
 imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 
 const favoriteFoods = ["chicken", "pasta", "beef"];
 
 
+const newFavoriteFood1 = "sushi";
+const newFavoriteFood2 = "pizza";
+
+
+favoriteFoods.push(newFavoriteFood1);
+favoriteFoods.push(newFavoriteFood2);
+
+
 foodElement.innerHTML = favoriteFoods.join("<br>");
-
-
-const newFavoriteFood = "sushi";
-
-
-favoriteFoods.push(newFavoriteFood);
-
-
-foodElement.innerHTML += "<br>" + favoriteFoods.join("<br>");
 
 
 favoriteFoods.shift();
@@ -45,6 +56,5 @@ favoriteFoods.pop();
 
 
 foodElement.innerHTML += "<br>" + favoriteFoods.join("<br>");
-
 
 
