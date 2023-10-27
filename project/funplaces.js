c67879c1412c5efcdc009e8975f91935d32bc202
AMbonani <refilwe.given@gmail.com>
2023-10-27 02:17:46 +0200

    project enhancement

--- project/funplaces.js
+++ project/funplaces.js
@@ -1,31 +1,79 @@
-const placeList = document.getElementById("placeList");
 
-function renderPlaces(places) {
-    placeList.innerHTML = "";
-
-    places.forEach(place => {
-        const item = document.createElement("li");
-        item.innerHTML = `
-            <h2>${place.name}</h2>
-            <p>Location: ${place.location}</p>
-            <p>Category: ${place.category}</p>
-            <img src="${place.image}" alt="${place.name}">
-        `;
-        placeList.appendChild(item);
-    });
-}
-
-async function fetchPlaces() {
+const fetchData = async () => {
     try {
-        const response = await fetch('fun.json'); ile
+        const response = await fetch('fun.json'); 
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        renderPlaces(data.places);
+        return data;
     } catch (error) {
         console.error('Error fetching data:', error);
+        return [];
     }
-}
+};
 
-fetchPlaces();
+
+const populatePlaceList = (places) => {
+    const placeList = document.getElementById('placeList');
+    placeList.innerHTML = '';
+
+    places.forEach(place => {
+        const item = document.createElement('li');
+        item.innerHTML = `
+            <h2>${place.name}</h2>
+            <p>Location: ${place.location}</p>
+            <p>Category: ${place.category}</p>
+            <img src="images/${place.image}" alt="${place.name} Image">
+        `;
+        placeList.appendChild(item);
+    });
+};
+
+
+const populateGlossary = () => {
+    const glossaryList = document.getElementById('glossaryList');
+    const glossaryItems = [
+        {
+            term: 'Amusement Park',
+            description: 'A place with rides and games for kids.',
+            link: 'https://www.goldreefcity.co.za/theme-park/',
+            image: 'gcity.png'
+        },
+        {
+            term: 'Zoo',
+            description: 'A facility where kids can see and learn about animals.',
+            link: 'https://www.pretoriazoo.org/',
+            image: 'zoo.png'
+        },
+        {
+            term: 'Playground',
+            description: 'An outdoor area with equipment for kids to play on.',
+            link: 'https://cedarjunction.co.za/',
+            image: 'zitapark.png'
+        }
+    ];
+
+    glossaryItems.forEach(item => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <strong>${item.term}:</strong> ${item.description}
+            <br>
+            <a href="${item.link}" target="_blank">Visit ${item.term}</a>
+            <br>
+            <img src="images/${item.image}" alt="${item.term} Image">
+        `;
+        glossaryList.appendChild(li);
+    });
+};
+
+// Main function to fetch data and populate the content
+const main = async () => {
+    const data = await fetchData();
+    if (data.length > 0) {
+        populatePlaceList(data);
+    }
+    populateGlossary();
+};
+
+main();
